f0febb70e4290a8c6b3d2bb6103b57a372b044eb
Saratu Muazu <sarahembee71@gmail.com>
2020-07-18 10:45:08 +0100

    Fix Employer Dashboard

--- Controllers/employer/employer-dashboard.js
+++ Controllers/employer/employer-dashboard.js
@@ -4,28 +4,36 @@ const { errorResMsg, successResMsg } = require('../../Utils/response');
 // Get EMPLOYER DASHBOARD
 exports.dashboard = async (req, res) => {
   try {
+    // GET All EMPLOYERS COUNT 
     const allEmployees = await model.Employee.count();
-
+     
+    // GET Total Number of EmployeeContacted in Team
     const employeeContacted = await model.Team.count({
       where: {
         employer_id: req.body.employer_id,
       },
     });
 
+    // GET Total Number of EmployeeEmployed in Team 
     const employeeEmployed = await model.Team.count({
       where: {
         employer_id: req.body.employer_id,
         status: 'Accepted',
       },
     });
-
+   
+    // GET one Employer Detail
     const employer = await model.Employer.findOne({
       where: {
         employer_id: req.body.employer_id,
       },
     });
-
-    const data = await [{ all_employee: allEmployees }, { employee_contacted: employeeContacted }, { employee_employed: employeeEmployed }, { employer_details: employer }];
+// All Employees, EmployeeContacted, EmployeeEmployeed and EmployerDetails 
+    const data = await [
+      { all_employee: allEmployees }, 
+      { employee_contacted: employeeContacted }, 
+      { employee_employed: employeeEmployed }, 
+      { employer_details: employer }];
 
     if (!data) {
       return errorResMsg(res, 404, 'Data not found');
